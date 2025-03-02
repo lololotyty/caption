27de10d8d20c1ceb1b478c8a2ebc4a09575febd7
wddwwdw <hangerskype@gmail.com>
2025-03-03 02:00:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,21 +15,21 @@ class Rkn_Bots(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # start_pic
-    RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
+    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/94027ad785c6ba022fcd0-1d4e0c2f339471ce84.jpg")
 
     # wes response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
 
     # force subs channel ( required.. 😥)
-    FORCE_SUB = os.environ.get("FORCE_SUB", "Rkn_Botz") 
+    FORCE_SUB = os.environ.get("FORCE_SUB", "save_restricted_botss") 
     
     # database config ( required.. 😥)
     DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
     DB_URL = os.environ.get("DB_URL", "")
 
     # default caption 
-    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https//:t.me/Rkn_Botz'>{file_name} Main Telegram Channel: @RknDeveloper</a></b>",
+    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https://telegram.dog/Talk2support_bot'>{file_name} Main Telegram Channel: https://t.me/save_restricted_botss</a></b>",
     )
 
     # sticker Id
